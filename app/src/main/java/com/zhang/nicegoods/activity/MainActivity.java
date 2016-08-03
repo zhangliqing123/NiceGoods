27ed7d0d05613a984e3923ef4698c7b369975de1
zhangliqing123 <2278562199@qq.com>
2016-08-03 12:36:04 +0800

    第一次张立庆

---
+++ app/src/main/java/com/zhang/nicegoods/activity/MainActivity.java
@@ -0,0 +1,14 @@
+package com.zhang.nicegoods.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.zhang.nicegoods.R;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
